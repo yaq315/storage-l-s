91569540eab66a917e00e8da41d51f8fde0777bc
yaqootgharaibeh <yagoutgharaibeh@gmail.com>
2025-01-13 21:03:54 +0300

    :storage

---
+++ local storage/local.js
@@ -0,0 +1,17 @@
+
+// // set
+// window.localStorage.setItem("color","red")
+// window.localStorage.fontweight="blod"
+// window.localStorage["fontsize"]="20px"
+
+// //get
+// console.log(window.localStorage.getItem("color"))
+// console.log(window.localStorage.color)
+// console.log(window.localStorage["color"])
+
+// //remove
+// window.localStorage.removeItem("color")
+// console.log(typeof window.localStorage)
+
+// //set color in bage 
+// // document.body.style.background=
